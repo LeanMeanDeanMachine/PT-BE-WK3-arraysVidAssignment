14827668992ddf1ad1e1c7b94332ed3b677e8087
LeanMeanDeanMachine <126035068+LeanMeanDeanMachine@users.noreply.github.com>
2023-03-11 10:43:33 -0700

    a.	Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.

--- src/NamesArrays.java
+++ src/NamesArrays.java
@@ -1,5 +1,13 @@
 public class NamesArrays {
     public static void  main(String[] args) {
         String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+
+        //Loop to iterate through array and calculate average number of letters per name.
+        int totalLetters = 0;
+        for (String name : names) {
+            totalLetters += name.length();
+        }
+        double average = (double) totalLetters / names.length;
+        System.out.println(average);
     }
 }
