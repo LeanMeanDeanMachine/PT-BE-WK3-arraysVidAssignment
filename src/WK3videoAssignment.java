bb66c644ad7e62677154255911f61fe1ffe2ba76
LeanMeanDeanMachine <126035068+LeanMeanDeanMachine@users.noreply.github.com>
2023-03-11 10:26:16 -0700

    Coding Steps — Arrays and Methods 1.	Create an array of int called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93. a.	Programmatically subtract the value of the first element in the array from the value in the last element of the array (i.e. do not use ages[7] in your code). Print the result to the console.

---
+++ src/WK3videoAssignment.java
@@ -0,0 +1,11 @@
+public class WK3videoAssignment {
+    public static void main(String[] args) {
+        int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
+        int last = ages.length - 1; //last index of ages array
+
+        int result = ages[last] - ages[0];
+        System.out.println(result);
+
+
+    }
+}
