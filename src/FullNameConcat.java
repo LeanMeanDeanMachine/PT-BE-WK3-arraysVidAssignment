7a230b6d11c08429abfa327086d68151227a62b1
LeanMeanDeanMachine <126035068+LeanMeanDeanMachine@users.noreply.github.com>
2023-03-11 11:41:59 -0700

    8.	Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).

---
+++ src/FullNameConcat.java
@@ -0,0 +1,14 @@
+public class FullNameConcat {
+        public static void main(String[] args) {
+            String firstName = "Robert";
+            String lastName = "Dean";
+            String fullName = getFullName(firstName, lastName);
+            System.out.println(fullName);
+        }
+
+        public static String getFullName(String firstName, String lastName) {
+            return firstName + " " + lastName;
+        }
+    }
+
+
